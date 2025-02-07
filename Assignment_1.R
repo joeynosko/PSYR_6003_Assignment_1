9e975be0610c98f22497516a1633324b9da56167
Joey Nosko <joey.nosko@gmail.com>
2025-02-07 16:17:26 -0400

    Summary stats for nosuper_died as a whole sample and based on north/south.

--- Assignment_1.R
+++ Assignment_1.R
@@ -13,3 +13,18 @@ write.csv(nosupedead, "avengers_nosuper_died.csv", row.names=F)#saving data of t
 install.packages("foreign")
 library(foreign)
 write.foreign(nosupedead, "avengers_nosuper_died.sav", "avengers_nosuper_died.sps", package=c("SPSS"))#saving data of those w/o superpowers who died to SPSS file
+#summarize to understand the properties of combat effectiveness, kills, and injuries. Mean, SD, range. 
+reframe(nosupedead, mean(CombatEffectiveness), sd(CombatEffectiveness), range(CombatEffectiveness))#mean, sd, and range of combat effectiveness. got warning to use reframe() instead of summarize()
+reframe(nosupedead, mean(kills), sd(kills), range(kills))#mean, sd, and range of kills. 
+reframe(nosupedead, mean(injuries), sd(injuries), range(injuries))#mean, sd, and range of injuries. 
+#dividing the data into north and south. 
+nosupedead_N<- filter(nosupedead, north_south == "north")
+nosupedead_S<- filter(nosupedead, north_south == "south")
+#summary stats for north group 
+reframe(nosupedead_N, mean(CombatEffectiveness), sd(CombatEffectiveness), range(CombatEffectiveness))
+reframe(nosupedead_N, mean(kills), sd(kills), range(kills))
+reframe(nosupedead_N, mean(injuries), sd(injuries), range(injuries))
+#summary stats for south group 
+reframe(nosupedead_S, mean(CombatEffectiveness), sd(CombatEffectiveness), range(CombatEffectiveness))
+reframe(nosupedead_S, mean(kills), sd(kills), range(kills))
+reframe(nosupedead_S, mean(injuries), sd(injuries), range(injuries))
