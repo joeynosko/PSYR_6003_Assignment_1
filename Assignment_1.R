6cdcd0371d50042b2cdb53b619376eba9c5e196d
Joey Nosko <joey.nosko@gmail.com>
2025-02-15 20:32:17 -0400

    Finished (not finalized) data analysis script.

--- Assignment_1.R
+++ Assignment_1.R
@@ -20,11 +20,32 @@ reframe(nosupedead, mean(injuries), sd(injuries), range(injuries))#mean, sd, and
 #dividing the data into north and south. 
 nosupedead_N<- filter(nosupedead, north_south == "north")
 nosupedead_S<- filter(nosupedead, north_south == "south")
-#summary stats for north group 
+#summary stats (mean, sd, & range) for combat effectiveness, kills, and injuries for the North group
 reframe(nosupedead_N, mean(CombatEffectiveness), sd(CombatEffectiveness), range(CombatEffectiveness))
 reframe(nosupedead_N, mean(kills), sd(kills), range(kills))
 reframe(nosupedead_N, mean(injuries), sd(injuries), range(injuries))
-#summary stats for south group 
+#summary stats (mean, sd, & range) for combat effectiveness, kills, and injuries for the South group 
 reframe(nosupedead_S, mean(CombatEffectiveness), sd(CombatEffectiveness), range(CombatEffectiveness))
 reframe(nosupedead_S, mean(kills), sd(kills), range(kills))
 reframe(nosupedead_S, mean(injuries), sd(injuries), range(injuries))
+#Question 8 
+install.packages("pwr")#installs package needed for power analyses
+library(pwr)
+pwr.t.test(d = .3, sig.level = 0.05, power = .95)#gives us the recommended sample for our specified t-test. 
+#the recommended sample size in each group is 234.4627
+#Question 9 
+library(TOSTER)
+library(dplyr)
+avengers_no_NA %>% count(superpower)#figuring out the numbers in each group (superpowers and no superpowers)
+x <- c(32, 780)#creating a vector so both sample sizes (superpowers = yes and superpowers = no) can be inputted into the calculation. 
+power_t_TOST(n = 235, low_eqbound = -0.3, high_eqbound = 0.3, alpha = 0.05, type = "two.sample")#calculating power of our test to find a null effect, defined here as an effect size ranging from a d of -0.3 to 0.3. 
+#the power of this test is 0.8909247
+#Question 10 
+library(effectsize)
+#assigning the test statistic and sample sizes to variables to be used in effect size conversion. 
+t <- 4.25
+n1 <- 32
+n2 <- 780
+t_to_d(t, df_error = n1+n2-2, paired = FALSE)#converts test statistic into effect size with 95% confidence interval. 
+cohen.ES(test="t", size = "small")#reminds me what a small effect size is. 
+#d = 0.30 [0.16, 0.44] - this is a SMALL effect size using Cohen's (1982) convention.
